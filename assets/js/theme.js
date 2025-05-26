adeaa1ce11d5573d56c6ab2c82463f4df0580e1c
Yarden Rishoni <yarden.rishoni03@gmail.com>
2025-05-26 15:16:27 +0300

    changing the page navigate to top delay

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,12 +1,12 @@
-(function($) {
+(function ($) {
     'use strict';
-	
-	// Mobile Menu
+
+    // Mobile Menu
     $('.mobile-menu nav').meanmenu({
         meanScreenWidth: "991",
         meanMenuContainer: ".mobile-menu",
         onePage: false,
-    });	
+    });
 
     // sticky
     var wind = $(window);
@@ -21,10 +21,10 @@
     });
 
     // Script
-        $(".team-icon").click(function(){
-            $(this).siblings(".team-socail-icon").toggleClass('active');
-        });
-        
+    $(".team-icon").click(function () {
+        $(this).siblings(".team-socail-icon").toggleClass('active');
+    });
+
     // Loder  //
     $(function () {
         $('body').addClass('loaded');
@@ -62,7 +62,7 @@
                 items: 1
             }
         }
-    }) 
+    });
     // testimonial Active
     $('.testimonial_list2').owlCarousel({
         loop: true,
@@ -94,7 +94,7 @@
                 items: 1
             }
         }
-    }) 
+    });
 
     // galary Active
     $('.case-list').owlCarousel({
@@ -122,7 +122,7 @@
                 items: 5
             }
         }
-    }) 
+    });
 
 
     // brand Active
@@ -156,10 +156,10 @@
                 items: 4
             }
         }
-    })  
+    });
 
 
-	/*---------------------
+    /*---------------------
     WOW active js 
     --------------------- */
     new WOW().init();
@@ -170,7 +170,7 @@
         time: 1000
     });
 
-	
+
     // Venubox
 
     $('.venobox').venobox({
@@ -180,7 +180,7 @@
         infinigall: true
 
     });
-	/*--------------------------
+    /*--------------------------
      scrollUp
     ---------------------------- */
     $.scrollUp({
@@ -188,57 +188,57 @@
         easingType: 'linear',
         scrollSpeed: 900,
         animation: 'fade'
-    })
+    });
 
-        $(document).ready(function(){
-            $('#bar1').barfiller({ duration: 7000 });
-            $('#bar2').barfiller({ duration: 7000 });
-            $('#bar3').barfiller({ duration: 7000 });
-            $('#bar4').barfiller({ duration: 7000 });
-        });
-   
+    $(document).ready(function () {
+        $('#bar1').barfiller({ duration: 7000 });
+        $('#bar2').barfiller({ duration: 7000 });
+        $('#bar3').barfiller({ duration: 7000 });
+        $('#bar4').barfiller({ duration: 7000 });
+    });
 
-        //<!--jave script -->
-        
-        $(window).on('scroll', function () {
-            var scrolled = $(window).scrollTop();
-            if (scrolled > 300) $('.go-top').addClass('active');
-            if (scrolled < 300) $('.go-top').removeClass('active');
-        });
 
-        $('.go-top').on('click', function () {
-            $("html, body").animate({
-                scrollTop: "0"
-            }, 1200);
-        });
+    //<!--jave script -->
 
-        // Faq Accordion js
-        jQuery(document).ready(function($) {
-            "use strict";
+    $(window).on('scroll', function () {
+        var scrolled = $(window).scrollTop();
+        if (scrolled > 300) $('.go-top').addClass('active');
+        if (scrolled < 300) $('.go-top').removeClass('active');
+    });
 
-            $(".accordion > li:eq(0) a").addClass("active").next().slideDown();
+    $('.go-top').on('click', function () {
+        $("html, body").animate({
+            scrollTop: "0"
+        }, 200);
+    });
 
-            $(".accordion a").click(function (j) {
-                var dropDown = $(this).closest("li").find("p");
+    // Faq Accordion js
+    jQuery(document).ready(function ($) {
+        "use strict";
 
-                $(this).closest(".accordion").find("p").not(dropDown).slideUp();
+        $(".accordion > li:eq(0) a").addClass("active").next().slideDown();
 
-                if ($(this).hasClass("active")) {
-                    $(this).removeClass("active");
-                } else {
-                    $(this).closest(".accordion").find("a.active").removeClass("active");
-                    $(this).addClass("active");
-                }
+        $(".accordion a").click(function (j) {
+            var dropDown = $(this).closest("li").find("p");
 
-                dropDown.stop(false, true).slideToggle();
+            $(this).closest(".accordion").find("p").not(dropDown).slideUp();
 
-                j.preventDefault();
-            });
+            if ($(this).hasClass("active")) {
+                $(this).removeClass("active");
+            } else {
+                $(this).closest(".accordion").find("a.active").removeClass("active");
+                $(this).addClass("active");
+            }
 
+            dropDown.stop(false, true).slideToggle();
+
+            j.preventDefault();
         });
 
-     /* Portfolio Isotope  */
-    $('.image_load').imagesLoaded(function() {
+    });
+
+    /* Portfolio Isotope  */
+    $('.image_load').imagesLoaded(function () {
 
         if ($.fn.isotope) {
 
@@ -257,7 +257,7 @@
                 transitionDuration: '0.8s'
 
             });
-            $('.menu-filtering li').on('click', function() {
+            $('.menu-filtering li').on('click', function () {
 
                 $('.menu-filtering li').removeClass('current_menu_item');
 
@@ -275,60 +275,60 @@
 
         };
     });
-        
-        // Calender Jquery
-        var curDate = (new Date()).getDate();
-        var curMonth = (new Date()).getMonth();
-        var curYear = (new Date()).getFullYear();
-        var startDay = (new Date(curYear, curMonth, 1)).getDay();
-        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        var noofdays = ["31", "29", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"];
-        var prevMonth = noofdays[curMonth - 1];
-        if (curMonth == 11) {
-          prevMonth = noofdays[0];
-        } else if (curMonth == 0) {
-          prevMonth = noofdays[11];
-        }
-        var totalDays = noofdays[curMonth];
-        var counter = 0;
-        var precounter = prevMonth - (startDay - 1);
-        var rightbox = 6;
-        var flag = true;
 
-        jQuery('.curr-month b').text(months[curMonth]);
-        for (var i = 0; i < 42; i++) {
-          if (i >= startDay) {
+    // Calender Jquery
+    var curDate = (new Date()).getDate();
+    var curMonth = (new Date()).getMonth();
+    var curYear = (new Date()).getFullYear();
+    var startDay = (new Date(curYear, curMonth, 1)).getDay();
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var noofdays = ["31", "29", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"];
+    var prevMonth = noofdays[curMonth - 1];
+    if (curMonth == 11) {
+        prevMonth = noofdays[0];
+    } else if (curMonth == 0) {
+        prevMonth = noofdays[11];
+    }
+    var totalDays = noofdays[curMonth];
+    var counter = 0;
+    var precounter = prevMonth - (startDay - 1);
+    var rightbox = 6;
+    var flag = true;
+
+    jQuery('.curr-month b').text(months[curMonth]);
+    for (var i = 0; i < 42; i++) {
+        if (i >= startDay) {
             counter++;
             if (counter > totalDays) {
-              counter = 1;
-              flag = false;
+                counter = 1;
+                flag = false;
             }
             if (flag == true) {
-              jQuery('.all-date ul').append('<li class="monthdate">' + counter + '</li>');
+                jQuery('.all-date ul').append('<li class="monthdate">' + counter + '</li>');
             } else {
-              jQuery('.all-date ul').append('<li style="opacity:.8">' + counter + '</li>');
+                jQuery('.all-date ul').append('<li style="opacity:.8">' + counter + '</li>');
             }
-          } else {
+        } else {
             jQuery('.all-date ul').append('<li style="opacity:.8">' + precounter + '</li>');
             precounter++;
-          }
-
-          if (i == rightbox) {
-            jQuery(jQuery('.all-date ul li')[rightbox]).addClass("b-right");
-            rightbox = rightbox + 7;
-          }
-
-          if (i > 34) {
-            jQuery(jQuery('.all-date ul li')[i]).addClass("b-bottom");
-          }
-
-          if ((jQuery(jQuery('.all-date ul li')[i]).text() == curDate) && (jQuery(jQuery('.all-date ul li')[i]).css('opacity') == 1)) {
-            jQuery(jQuery('.all-date ul li')[i]).css({
-              "background-color": "#b59a6c",
-              "color": "#fff"
-            });
-          }
         }
 
-    
+        if (i == rightbox) {
+            jQuery(jQuery('.all-date ul li')[rightbox]).addClass("b-right");
+            rightbox = rightbox + 7;
+        }
+
+        if (i > 34) {
+            jQuery(jQuery('.all-date ul li')[i]).addClass("b-bottom");
+        }
+
+        if ((jQuery(jQuery('.all-date ul li')[i]).text() == curDate) && (jQuery(jQuery('.all-date ul li')[i]).css('opacity') == 1)) {
+            jQuery(jQuery('.all-date ul li')[i]).css({
+                "background-color": "#b59a6c",
+                "color": "#fff"
+            });
+        }
+    }
+
+
 })(jQuery);
